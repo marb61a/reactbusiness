825e2d53ba985614b1bb55985696beb9ac631b7b
marb <marb61a@gmail.com>
2016-02-02 11:25:40 +0000

    added business js component

---
+++ src/js/components/Business.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var BusinessStore = require('../stores/BusinessStore');
+
+var Business = React.createClass({
+	render: function(){
+		return(
+				<div>
+					BUSINESS
+				</div>
+		);
+	}
+});
+
+module.exports = Business;
