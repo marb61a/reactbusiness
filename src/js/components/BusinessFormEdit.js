9f2891e88b07141c8c972d95307872c36494246e
marb <marb61a@gmail.com>
2016-02-03 11:17:12 +0000

    added business form

---
+++ src/js/components/BusinessFormEdit.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var BusinessStore = require('../stores/BusinessStore');
+
+var BusinessFormEdit = React.createClass({
+	render: function(){
+		return(
+				<div>
+					BUSINESS FORM EDIT
+				</div>
+			)
+	}
+});
+
+module.exports = BusinessFormEdit;
