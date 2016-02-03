d9f628ceb5dc8064ad28c666e571707f248f446a
marb <marb61a@gmail.com>
2016-02-03 11:33:57 +0000

    added business list component

---
+++ src/js/components/BusinessList.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var BusinessStore = require('../stores/BusinessStore');
+var Business = require('./Business.js');
+
+var BusinessList = React.createClass({
+	render: function(){
+		return(
+				<div>
+					BUSINESSLIST
+				</div>
+			)
+	}
+});
+
+module.exports = BusinessList;
