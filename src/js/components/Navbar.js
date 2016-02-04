0daaa8fe26439fe00c495e570002a66430faaf85
marb <marb61a@gmail.com>
2016-02-04 11:04:12 +0000

    added navbar component

---
+++ src/js/components/Navbar.js
@@ -0,0 +1,34 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var BusinessStore = require('../stores/BusinessStore');
+
+var Navbar = React.createClass({
+    render : function(){
+        return(
+            <div>
+                <nav className="navbar navbar-default">
+			      <div className="container">
+			        <div className="navbar-header">
+			          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			            <span className="sr-only">Toggle navigation</span>
+			            <span className="icon-bar"></span>
+			            <span className="icon-bar"></span>
+			            <span className="icon-bar"></span>
+			          </button>
+			          <a className="navbar-brand" href="#">BizList</a>
+			        </div>
+			        <div id="navbar" className="collapse navbar-collapse">
+			          <ul className="nav navbar-nav">
+			            <li className="active"><a href="#">Home</a></li>
+			            <li><a href="#">Add Business</a></li>
+			          </ul>
+			        </div>
+			      </div>
+			    </nav>
+            </div>
+        );
+    }
+});
+
+
+module.exports = Navbar;
