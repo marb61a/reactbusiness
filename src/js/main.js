27c08c7f11fffa4dc5592f9597009af13b6b3241
marb <marb61a@gmail.com>
2016-02-10 11:21:31 +0000

    updated main js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/App');
+var AppAPI = require('./utils/appAPI.js');
+var StartData = require('./startdata.js');
+
+if(localStorage.getItem('businesses') == null){
+	StartData.init();
+}
+
+// Get Items From API
+AppAPI.getAllItems();
 
 ReactDOM.render(
 	<App />,
