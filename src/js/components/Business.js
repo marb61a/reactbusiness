a8f1dd085999f8bc4d44c2ba3ada266e49d59e4f
marb <marb61a@gmail.com>
2016-02-11 14:31:04 +0000

    added business extended to business.js

--- src/js/components/Business.js
+++ src/js/components/Business.js
@@ -27,4 +27,43 @@ var Business = React.createClass({
 	}
 });
 
+var businessExtended = React.createClass({
+	render : function(){
+		return(
+			<div className="row">
+				<hr/>
+				<div className="col-md-6">
+					<h3>Location</h3>
+					<ul className="list-group">
+						<li className="list-group-item">{this.props.businessInfo.address.street} </li>
+						<li className="list-group-item">{this.props.businessInfo.address.city}  
+						 {this.props.businessInfo.address.state}, 
+						{this.props.businessInfo.address.zipcode}</li>
+					</ul>
+					<a className="btn btn-primary btn-block" href="#">Email Us</a>
+				</div>
+				<div className="col-md-6">
+					<h3>About Us</h3>
+					<p className="well">{this.props.businessInfo.description}</p>
+				</div>
+				<br/>
+				<div className="col-md-2 col-md-offset-10">
+					<div className="btn-group btn-group" role="group" aria-label="Large button group">
+						<button type="button" onClick={this.handleEditClick.bind(this, this.props.businessInfo.id)} className="btn btn-primary">Edit</button>
+						<button type="button" onClick={this.handleRemoveClick.bind(this, this.props.businessInfo.id)} className="btn btn-danger">Delete</button>
+				    </div>
+				</div>
+			</div>	
+		);
+	},
+	
+	handleEditClick : function(i, j){
+		
+	},
+	
+	handleRemoveClick : function(i, j){
+		
+	}
+});
+
 module.exports = Business;
