2d1d94914baa8b5eeef17d3bea0ad08968fbc209
marb <marb61a@gmail.com>
2016-02-11 14:11:17 +0000

    updated app actions

--- src/js/actions/AppActions.js
+++ src/js/actions/AppActions.js
@@ -2,7 +2,31 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 
 var AppActions = {
-
-}
+    receiveItems: function(items){
+		AppDispatcher.handleViewAction({
+			actionType: AppConstants.RECEIVE_ITEMS,
+			items: items
+		});
+	},
+	
+	newItem: function(items){
+		AppDispatcher.handleViewAction({
+			actionType: AppConstants.NEW_ITEM
+		});
+	},
+	
+	cancelItem: function(items){
+		AppDispatcher.handleViewAction({
+			actionType: AppConstants.CANCEL_ITEM
+		});
+	},
+	
+	extendItem: function(itemId){
+		AppDispatcher.handleViewAction({
+			actionType: AppConstants.EXTEND_ITEM,
+			itemId: itemId
+		});
+	}
+};
 
 module.exports = AppActions;
