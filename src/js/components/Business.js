6b0d8b338376860701f0951c1a00ef8b418509f9
marb <marb61a@gmail.com>
2016-02-11 14:19:37 +0000

    updated business.js var business function

--- src/js/components/Business.js
+++ src/js/components/Business.js
@@ -4,11 +4,26 @@ var BusinessStore = require('../stores/BusinessStore');
 
 var Business = React.createClass({
 	render: function(){
+		var businessExtended;
+		
+		if(this.props.showExtended && this.props.selectedId == this.props.businessInfo.id){
+			businessExtended = <BusinessExtended businessInfo= {this.props.businessInfo} />;
+		}else{
+			businessExtended = '';
+		}
+		
 		return(
-				<div>
-					BUSINESS
+				<div className="well">
+					<button onClick={this.handleMoreClick.bind(this, this.props.businessInfo.id)} className="btn btn-default pull-right">Show More</button>
+					<h3>{this.props.businessInfo.name}</h3>
+					<small>Category: {this.props.businessInfo.category}</small>
+					{businessExtended}
 				</div>
 		);
+	},
+	
+	handleMoreClick : function(i, j){
+		AppActions.extendItem(i);
 	}
 });
 
