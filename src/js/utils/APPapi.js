0b69db3f569ee3972d3e5d915fcd52eea3d58b2c
marb <marb61a@gmail.com>
2016-02-19 15:07:10 +0000

    updated APPapi with save and remove item

--- src/js/utils/APPapi.js
+++ src/js/utils/APPapi.js
@@ -5,6 +5,22 @@ module.exports = {
 		var items = JSON.parse(localStorage.getItem('businesses'));
 		// Simulates Success Callback
 		AppActions.receiveItems(items);
+	},
+	
+	saveItem: function(item){
+		var items = JSON.parse(localStorage.getItem('businesses'));
+		items.push(item);
+		localStorage.setItem('businesses', JSON.stringify(items));
+	},
+	
+	removeItem: function(itemId){
+		var items = JSON.parse(localStorage.getItem('businesses'));
+		for(i = 0; i <items.length; i++){
+			if(items[i].id == itemId){
+				items.splice(i, 1);
+			}
+		}
+		localStorage.setItem('businesses', JSON.stringify(items));
 	}
 	
 };
