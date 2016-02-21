bcdd77f59076381e1c66fa1bde9b78f2e89438e8
marb <marb61a@gmail.com>
2016-02-21 18:59:33 +0000

    updated business edit form with initial values

--- src/js/components/BusinessFormEdit.js
+++ src/js/components/BusinessFormEdit.js
@@ -3,12 +3,29 @@ var AppActions = require('../actions/AppActions');
 var BusinessStore = require('../stores/BusinessStore');
 
 var BusinessFormEdit = React.createClass({
+	componentDidMount: function(){
+		BusinessStore.getBusiness(this.props.selectedId);
+	},
+	
 	render: function(){
 		return(
-				<div>
-					BUSINESS FORM EDIT
+				<div className="well">
+					<h3>Edit Business</h3>
+					<form onSubmit={this.handleSubmit}>
+						<div className="form-group">
+							<label htmlFor="item.name">
+								Business Name
+							</label>
+							<input type="text" ref="name" className="form-control" onChange={this.handleChange.bind(this, 'name')} value={this.props.selected.name} placeholder="Business Name" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="category">
+								Category
+							</label>
+						</div>
+					</form>
 				</div>
-			)
+			);
 	}
 });
 
