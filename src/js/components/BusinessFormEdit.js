a3dc566b697e7689e655f1366ae3b00d4d8b68ab
marb <marb61a@gmail.com>
2016-02-22 10:56:25 +0000

    updated business edit form with form values

--- src/js/components/BusinessFormEdit.js
+++ src/js/components/BusinessFormEdit.js
@@ -22,7 +22,30 @@ var BusinessFormEdit = React.createClass({
 							<label htmlFor="category">
 								Category
 							</label>
+							<select ref="category" className="form-control">
+						    	<option value="0">Select Category</option>
+						    	<option value="Construction" selected={this.props.selected.category == 'Construction'}>Construction</option>
+						    	<option value="Food" selected={this.props.selected.category == 'Food'}>Food</option>
+						    	<option value="Fashion" selected={this.props.selected.category == 'Fashion'}>Fashion</option>
+						    	<option value="Finance" selected={this.props.selected.category == 'Finance'}>Finance</option>
+						    	<option value="Retail" selected={this.props.selected.category == 'Retail'}>Retail</option>
+						    	<option value="Technology" selected={this.props.selected.category == 'Technology'}>Technology</option>
+						    	<option value="Other" selected={this.props.selected.category == 'Other'}>Other</option>
+					    	</select>
 						</div>
+						<div className="form-group">
+							<label htmlFor="street_address">
+								Street Address
+							</label>
+					    	<input type="text" ref="street" onChange={this.handleChange.bind(this, 'address.street')} value={this.props.selected.address.street} 
+					    	className="form-control" placeholder="Street Address" />
+						</div>
+						<div className="form-group">
+						    <label htmlFor="city">
+						    	City
+						    </label>
+						    <input type="text" ref="city" className="form-control" onChange={this.handleChange.bind(this, 'city')} value={this.props.selected.address.city} placeholder="City" />
+					  	</div>
 					</form>
 				</div>
 			);
